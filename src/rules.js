4d1ebdac38b1ab3bb36730a9f2ed6ef7eb47d5ac
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 14:39:18 -0800

    copy html into file in comments

---
+++ src/rules.js
@@ -0,0 +1,19 @@
+
+
+
+/*
+<div id="rules-div">
+  <ul>
+    <li>One mulligan per round</li>
+    <li>Ready golf</li>
+    <li>Take a provisional</li>
+    <li>Gimmes are up to group consensus</li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</div>
+*/
