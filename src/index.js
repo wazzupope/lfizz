36699352e1e56eee739e9b1d1859d4c18c11a07d
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 15:51:43 -0800

    add variables for title and nav links, as variables in their js files are not in scope

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ import {createStandings, loadStandings} from './standings.js';
 
 loadHome();
 
+// Add event listeners for title and nav
+const title = document.getElementById('title');
+title.addEventListener('click', loadHome);
+
+const rulesBox = document.getElementById('rules-box');
 rulesBox.addEventListener('click', loadRules);
+
+const historyBox = document.getElementById('history-box');
 historyBox.addEventListener('click', loadHistory);
+
+const pairingsBox = document.getElementById('pairings-box');
 pairingsBox.addEventListener('click', loadPairings);
+
+const standingsBox = document.getElementById('standings-box');
 standingsBox.addEventListener('click', loadStandings);
