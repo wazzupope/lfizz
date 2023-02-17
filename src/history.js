606405eeef68bc8c69da87ef97dce91cc38895c6
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 15:37:03 -0800

    add while loop to clear dom and add history page content

--- src/history.js
+++ src/history.js
@@ -3,6 +3,19 @@ function createHistory() {
   history.textContent = "History";
 };
 
+function loadHistory() {
+  const content = document.getElementById("content");
+
+  while (content.lastChild) {
+    content.lastChild.remove();
+  }
+
+  content.appendChild(createHeader());
+  content.appendChild(createNav());
+  content.appendChild(createHistory());
+};
+
 export {
-  createHistory
+  createHistory,
+  loadHistory
 };
