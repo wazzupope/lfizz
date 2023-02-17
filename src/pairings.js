7bca06aae870617b329b5bc01c7a293cc43074b2
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 15:40:17 -0800

    add while loop to clear dom and add pairings page content

--- src/pairings.js
+++ src/pairings.js
@@ -3,6 +3,19 @@ function createPairings() {
   pairings.textContent = "Pairings";
 };
 
+function loadPairings() {
+  const content = document.getElementById("content");
+
+  while (content.lastChild) {
+    content.lastChild.remove();
+  }
+
+  content.appendChild(createHeader());
+  content.appendChild(createNav());
+  content.appendChild(createPairings());
+};
+
 export {
-  createPairings
+  createPairings,
+  loadPairings
 };
