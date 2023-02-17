f92bfb4d83846eae3158c6ea78559fddcbd85bf3
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 14:43:46 -0800

    create file for standings page and add comments on plan for page

---
+++ src/standings.js
@@ -0,0 +1,2 @@
+// do a true leaderboard, sortable, with gross score, net score, 9 hole scores broken down by round and front/back, # of snowmen, # of birdies
+// modify view for mobile so that you can scroll right to see sortable categories, when pushing sort on one, it doesn't scroll back to the left
