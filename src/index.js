33c72f83c489c8f48f7954807ebc96d9120f52b5
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 07:24:22 -0800

    add elements to main with appropriate ids and classes and nesting

--- src/index.js
+++ src/index.js
@@ -92,48 +92,36 @@ function createNav() {
 };
 
 function createMain() {
+  const main = document.createElement('main');
 
+  const mainSection = document.createElement('section');
+  mainSection.setAttribute('id', 'main-section');
+
+  const date = document.createElement('h2');
+  date.setAttribute('id', 'date');
+  date.textContent = "July 1, 2023";
+
+  const course = document.createElement('h3');
+  course.setAttribute('id', 'course');
+  course.textContent = "Elk Ridge Golf Course";
+
+  const street = document.createElement('div');
+  street.classList.add('contact-info');
+  street.textContent = "1 Saint Martin's Springs Road";
+
+  const town = document.createElement('div');
+  town.classList.add('contact-info');
+  town.textContent = "Carson, WA 98610";
+
+  const phone = document.createElement('div');
+  phone.classList.add('contact-info');
+  phone.textContent = "(509) 427-0127";
+
+// Nesting Main Elements
+  mainSection.appendChild(date);
+  mainSection.appendChild(course);
+  mainSection.appendChild(street);
+  mainSection.appendChild(town);
+  mainSection.appendChild(phone);
+  main.appendChild(mainSection);
 };
-
-/*
-<header>
-  <a href="index.html">
-    <h1 id="title">LFizzle Invitational</h1>
-  </a>
-</header>
-<nav>
-  <a href="rules.html">
-    <div id="rules-box" class="nav-boxes">
-      <div id="rules-icon" class="nav-icons">icon</div>
-      <span id="rules-text" class="nav-text">Rules</span>
-    </div>
-  </a>
-  <a href="history.html">
-    <div id="history-box" class="nav-boxes">
-      <div id="history-icon" class="nav-icons">icon</div>
-      <span id="history-text" class="nav-text">History</span>
-    </div>
-  </a>
-  <a href="pairings.html">
-    <div id="pairings-box" class="nav-boxes">
-      <div id="pairings-icon" class="nav-icons">icon</div>
-      <span id="pairings-text" class="nav-text">Pairings</span>
-    </div>
-  </a>
-  <a href="standings.html">
-    <div id="standings-box" class="nav-boxes">
-      <div id="standings-icon" class="nav-icons">icon</div>
-      <span id="standings-text" class="nav-text">Standings</span>
-    </div>
-  </a>
-</nav>
-<main>
-  <section id="main-section">
-    <h2 id="date">July 1, 2023</h2>
-    <h3 id="course">Elk Ridge Golf Course</h3>
-    <div class="contact-info">1 Saint Martin's Springs Road</div>
-    <div class="contact-info">Carson, WA 98610</div>
-    <div class="contact-info">(509) 427-0127</div>
-  </section>
-</main>
-*/
