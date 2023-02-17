e53c1e32de595d725da8dce57897275d60faef5c
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 15:14:21 -0800

    create basic elements to appear on page to confirm file is working when run

--- src/standings.js
+++ src/standings.js
@@ -1,2 +1,11 @@
 // do a true leaderboard, sortable, with gross score, net score, 9 hole scores broken down by round and front/back, # of snowmen, # of birdies
 // modify view for mobile so that you can scroll right to see sortable categories, when pushing sort on one, it doesn't scroll back to the left
+
+function createStandings() {
+  const standings = document.createElement('div');
+  standings.textContent = "Standings";
+};
+
+export {
+  createStandings
+};
