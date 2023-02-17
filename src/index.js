77abe2927f431098d9416d65db8087688bd0c085
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 15:18:12 -0800

    import from history, pairings, and standings files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import {createHeader, createNav, createMain, loadHome} from './home.js';
 import {createRules} from './rules.js';
+import {createHistory} from './history.js';
+import {createPairings} from './pairings.js';
+import {createStandings} from './standings.js';
 
 loadHome();
