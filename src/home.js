687934e9e642764fe90ebeb0f0c4cf4ba377fe58
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-17 15:33:57 -0800

    add while loop to remove pre-existing dom elements in content before appending new page content

--- src/home.js
+++ src/home.js
@@ -133,6 +133,10 @@ function createMain() {
 function loadHome() {
   const content = document.getElementById("content");
 
+  while (content.lastChild) {
+    content.lastChild.remove();
+  }
+
   content.appendChild(createHeader());
   content.appendChild(createNav());
   content.appendChild(createMain());
