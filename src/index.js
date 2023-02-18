20b9f5eb35c91335829b1525892812fbac954327
jpfairhart <johnpaulfairhart@hotmail.com>
2023-02-18 07:51:01 -0800

    remove imports from index.js and move to home.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import {createHeader, createNav, createMain, loadHome} from './home.js';
-import {createRule, loadRules} from './rules.js';
-import {createHistory, loadHistory} from './history.js';
-import {createPairings, loadPairings} from './pairings.js';
-import {createStandings, loadStandings} from './standings.js';
+import {loadHome} from './home';
 
 loadHome();
